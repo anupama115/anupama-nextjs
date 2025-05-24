ee6fb143d5a25c15a81dd33d1852f69d65759484
anupamakarki36 <anupamakarki36@gmail.com>
2025-05-24 17:11:04 +1000

    changes made

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
+    <><div>
       <h1 className='text-3xl font-bold underline'>Contact Page</h1>
-    </div>
+    </div><div>My name is Saranch I am brilliant kiddddd</div></>
   )
 }
 
